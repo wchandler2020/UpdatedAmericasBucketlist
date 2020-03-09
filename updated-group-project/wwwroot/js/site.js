3160c3c511414a105a757b0d7c620236a401ca7f
fatima <tima.alice806@gmail.com>
2020-03-09 16:01:36 -0500

    added google maps functionality, created logo

--- updated-group-project/wwwroot/js/site.js
+++ updated-group-project/wwwroot/js/site.js
@@ -1,4 +1,28 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function initMap() {
+    //map..
+    var map = new google.maps.Map(document.getElementById('myMap'), {
+        center: {
+            lat: 10.9968683,
+            lng: 76.00882
+        },
+        zoom: 15
+    });
+    //marker..
+    var marker = new google.maps.Marker({
+        position: {
+            lat: 10.9968683,
+            lng: 76.00882
+        },
+        map: map,
+        draggable: true
+    });
 
-// Write your JavaScript code.
+    //dragend event of marker
+    google.maps.event.addListener(marker, 'dragend', function () {
+        var Lats = marker.getPosition().lat();
+        var Longs = marker.getPosition().lng();
+        $('#Lat').val(Lats);
+        $('#Long').val(Longs);
+    });
+}
+
