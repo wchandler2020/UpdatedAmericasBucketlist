baa52c5d30239aae3cf73a9ad9f070f1e9170791
fatima <tima.alice806@gmail.com>
2020-03-09 11:51:01 -0500

    google o auth API key test

---
+++ updated-group-project/Data/Migrations/20200309164746_migratingGoogleOauth20.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace updated_group_project.Data.Migrations
+{
+    public partial class migratingGoogleOauth20 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
