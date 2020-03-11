2efce41ed84be35334c291970c01b8200179d0eb
Matthew Acker <matthew.acker11b@gmail.com>
2020-03-11 15:46:44 -0500

    merging

--- updated-group-project/Data/ApplicationDbContext.cs
+++ updated-group-project/Data/ApplicationDbContext.cs
@@ -16,17 +16,6 @@ namespace updated_group_project.Data
         }
         public DbSet<EventDetails> EventDetails { get; set; }
         public DbSet<User> User { get; set; }
-<<<<<<< HEAD
-<<<<<<< HEAD
-        
-=======
-<<<<<<< HEAD
-        
-=======
->>>>>>> bf290455bf741048d9a861ea43534464592ac516
->>>>>>> 6bbd1a172bb2611a753be1824ee1a355f87c9657
-=======
 
->>>>>>> 6a405484ccd804595022bb4a73dfc52c686cd8a4
     }
 }
