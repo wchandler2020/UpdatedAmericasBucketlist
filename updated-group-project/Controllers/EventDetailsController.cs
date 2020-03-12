10d29ad4fbd607147eea79d13df9e01649ba888f
fatima <tima.alice806@gmail.com>
2020-03-12 17:26:12 -0500

    refactored all the views and included search methods

--- updated-group-project/Controllers/EventDetailsController.cs
+++ updated-group-project/Controllers/EventDetailsController.cs
@@ -20,32 +20,12 @@ namespace updated_group_project.Controllers
             _context = context;
         }
 
-        // GET: EventDetails
-
-        //public async Task<IActionResult> Index([FromServices] IEventService eventServices)
-        //{
-        //    EventObject eventFull = await eventServices.GetEvent();
-        //    _context.EventDetails.Where(c => c.title == );
-        //    return View(eventFull);
-        //}
-
         public async Task<IActionResult> Index([FromServices] IEventService eventServices)
         {
-            EventDetails eventDetails = new EventDetails();
-            EventObject eventFull = await eventServices.GetEvent();
-            foreach (Event e in eventFull.events.eventArray)
-            {
-                eventDetails.cityName = e.city_name;
-                eventDetails.desciption = e.description;
-                eventDetails.title = e.title;
-                eventDetails.startTime = e.start_time;
-                eventDetails.startTime = e.stop_time;
-                eventDetails.venueAddress = e.venue_address;
-                eventDetails.venueDisplay = e.venue_display;
-                //check if exists
-            }
-            _context.SaveChanges();
-            return View(eventDetails);
+            EventObject eventful = await eventServices.GetEvent();
+
+            return View(eventful);
+
         }
 
         // GET: EventDetails/Details/5
